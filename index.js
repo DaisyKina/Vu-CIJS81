df5e1d80c2a8ca3879f4f5cc7beac42923b704f6
DaisyKina <116964619+DaisyKina@users.noreply.github.com>
2023-02-23 19:18:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const a = 10
+
+const page = (
+    <div>
+        <p><u>Enter your task here...</u></p>
+        <ul>
+            <li>Clean up the bedroom</li>
+            <li>Buy some milk</li>
+            <li>Jogging</li>
+            <li>Learn react </li>
+            <li>Doing exercises</li>
+        </ul>
+    </div>
+)
+ReactDOM.render(page , document.getElementById("root"))
+
+// function Page(){
+//     return(
+//         <div> 
+//             <header>
+//                 <nav>
+//                     <img src="logo/logo-gold.jpg"></img>
+//                 </nav>
+//             </header>
+//             <h1>Reasons I don't like React</h1>
+//             <ol>
+//                 <li>Stupid as fuck</li>
+//                 <li>Stupid but double it</li>
+//             </ol>
+//             <footer>
+//                 <small> All rights reserved</small>
+//             </footer>
+//         </div>
+//     )
+// }
+// ReactDOM.render( Page, document.getElementById("root"))
